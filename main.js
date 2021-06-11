f3bf52493085c14a20e8d62c2bdffdc982012628
Devashishdas12345 <77055746+Devashishdas12345@users.noreply.github.com>
2021-06-11 19:06:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
+rightWristX = "";
+rightWristY = "";
+rigthWristScore = "";
+GameStatus = "";
+
 function setup() {
 	canvas = createCanvas(600 , 500);
 	canvas.parent('canvas');
-	video = createCapture(600,500);
+	video = createCapture(VIDEO);
+	video.size(600 , 500);
 	video.hide();
 
 	posenet = ml5.poseNet(video, modelLoaded);
 	posenet.on('pose', gotPoses);
 }
 
-function draw() {
-	image(video, 0 ,0 ,600 ,500)
+function modelLoaded() {
+  console.log("Model is Loaded!");
 }
 
-function modelLoaded() {
-  console.log("Model Loaded");
+function gotPoses(results) {
+	if (results.length > 0) {
+		rightWristX = results[0].pose.rightWrist.x;
+	    rightWristY = results[0].pose.rightWrist.y;
+		console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+		rigthWristScore =  results[0].pose.keypoints[10].score;
+		console.log(rigthWristScore);
+	}
+}
+
+function startGame() {
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game Is Loaded";
+}
+
+function draw() {
+	if (GameStatus == "start") {
+		image(video, 0 ,0 ,600 ,500);
+	
+	    if (rigthWristScore > 0.2) {
+			fill("#00ffae");
+			stroke("#00aaff");
+			circle(rightWristX + 15 , rightWristY + 15 , 20);
+		}
+	}
 }
